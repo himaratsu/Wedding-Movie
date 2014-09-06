ee01abd147806b8df4f4eb5e007e6600c19490f0
Ryosuke Hiramatsu <himaratsu@gmail.com>
2014-09-06 11:28:52 +0900

    add views/ and routing pc or mobile.

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 // all environments
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -21,6 +24,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -37,10 +41,13 @@ mongoose.connect(mongoUri);
 
 // routing
 var routes = {
+	index:require('./routes/index.js'),
 	v1: {
 		movies:require('./routes/v1/movies.js')
 	}
 };
+
+app.get('/', routes.index.index);
 
 app.get('/v1/movies', routes.v1.movies.get);
 app.post('/v1/movies/random', routes.v1.movies.postRandom);
